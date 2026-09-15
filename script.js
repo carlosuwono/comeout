f1858c99e702deebc82196a33ce831f1bdd87b65
carlosuwono <carlosuwono37@gmail.com>
2026-09-15 11:03:37 +1000

    added transition and styled the buttons etc

--- script.js
+++ script.js
@@ -5,10 +5,27 @@ const coverScreen = document.getElementById('Cover');
 const introScreen = document.getElementById('Intro');
 const button = document.getElementById("Start");
 
+const doorLeft = document.getElementById('LeftDoor');
+const doorRight = document.getElementById('RightDoor');
+
 
 button.addEventListener('click', function() {
+    doorLeft.classList.add('MoveLeft');
+    doorRight.classList.add('MoveRight');
+     
+    setTimeout(function() {
     coverScreen.classList.remove('Active');
+    }, 1000);
+   
     introScreen.classList.add('Active');
+
+    setTimeout(function() {
+    introScreen.classList.add('Show');
+    }, 1000);
+
+    setTimeout(function() {
+    introScreen.classList.add('Blur');
+    }, 1000);
 });
 
 });
